8990ce38ff12735ffc93d2140acb18ab91b59a61
swapnilg671 <swapnilg671@gmail.com>
2021-11-02 13:51:57 +0530

    product-web

---
+++ target/classes/com/javaEclipse/projectweb/CreateProductServlet.java
@@ -0,0 +1,23 @@
+package com.javaEclipse.projectweb;
+
+import java.io. ;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet
+public class CreateProductServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    
+    public CreateProductServlet() {
+       
+    }
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
